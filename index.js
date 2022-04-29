b786ec43abbf091de1b9e349b7a12e79c4f5e71e
Apoh <Edenra08@gmail.com>
2022-04-29 19:52:04 +0100

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+
+document.addEventListener("DOMContentLoaded", function() {
+   let textElement = document.getElementsByTagName("P")[0];
+   textElement.textContent = "This is really cool!"
+    console.log("The DOM has loaded");
+  });
